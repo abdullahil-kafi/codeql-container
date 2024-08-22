bc868801f324ba429c378a7cff67ce72aec05746
Md Abdullahil Kafi <abdullahil@openrefactory.com>
2024-08-23 00:55:36 +0600

    remove option for downloading and compiling new queries at runtime

--- container/startup.py
+++ container/startup.py
@@ -7,14 +7,14 @@ from libs.codeql import *
 CODEQL_HOME = get_env_variable('CODEQL_HOME')
 
 # should we update the local copy of codeql-cli if a new version is available?
-CHECK_LATEST_CODEQL_CLI = get_env_variable('CHECK_LATEST_CODEQL_CLI', False)
+CHECK_LATEST_CODEQL_CLI = get_env_variable('CHECK_LATEST_CODEQL_CLI', True)
 
 # should we update the local copy of codeql queries if a new version is available?
-CHECK_LATEST_QUERIES = get_env_variable('CHECK_LATEST_QUERIES', False)
+# CHECK_LATEST_QUERIES = get_env_variable('CHECK_LATEST_QUERIES', True)
 
 # if we are downloading new queries, should we precompile them
 #(makes query execution faster, but building the container build slower).
-PRECOMPILE_QUERIES = get_env_variable('PRECOMPILE_QUERIES', False)
+# PRECOMPILE_QUERIES = get_env_variable('PRECOMPILE_QUERIES', True)
 
 # ql packs, requested to run, if any
 CODEQL_CLI_ARGS = get_env_variable('CODEQL_CLI_ARGS', True)
@@ -28,10 +28,10 @@ def main():
     setup_script_args = ''
     if CHECK_LATEST_CODEQL_CLI:
         setup_script_args += ' --check-latest-cli'
-    if CHECK_LATEST_QUERIES:
-        setup_script_args += ' --check-latest-queries'
-    if PRECOMPILE_QUERIES:
-        setup_script_args += ' --precompile-latest-queries'
+    # if CHECK_LATEST_QUERIES:
+    #     setup_script_args += ' --check-latest-queries'
+    # if PRECOMPILE_QUERIES:
+    #     setup_script_args += ' --precompile-latest-queries'
 
     run_result = check_output_wrapper(
         f"{scripts_dir}/setup.py {setup_script_args}",
